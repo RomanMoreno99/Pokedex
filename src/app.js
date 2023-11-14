512c2a01e2476c6aff5cf2d144494dd428e0df4f
RMoreno <romanbarrionuevo143@gmail.com>
2023-11-13 22:34:18 -0300

    pokedex

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
+import mongoose from 'mongoose'
+
 import __dirname from './utils.js'
 import pokeRouter from './routes/poke.router.js'
 
 //Inicializamos las variables
 const app = express()
+const mongoURL = 'mongodb+srv://Roman2710:Amoamimami123@clusterroman.kkfusgx.mongodb.net/'
+const mongoDBName = 'clase15'
+
+//Para traer info de POST como JSON
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //Configurar el motor de la Plantilla
 app.engine('handlebars', handlebars.engine())
@@ -16,4 +24,10 @@ app.get('/', (req,res) => res.render('index', {}))
 app.get('/health', (req, res) => res.send('OK'))
 app.use('/pokemon', pokeRouter)
 
-app.listen(8080, () => console.log(`Listening 🏃 ....`))
+mongoose.connect(mongoURL, {dbName: mongoDBName})
+.then(() => {
+    console.log('DB connected B)')
+    app.listen(8080, () => console.log(`Listening 🏃 ....`))
+})
+.catch(e => console.log('Error to connected :/'))
+
--- src/routes/poke.router.js
+++ src/routes/poke.router.js
@@ -1,23 +1,54 @@
 import { Router } from 'express'
+import pokeModel from '../models/poke.models.js'
 
 const router = Router()
 
-router.get('/', async(req, res) => {
-    res.render('list', {pokemons: []})
+router.get('/', async (req, res) => {
+    const pokemons = await pokeModel.find().lean().exec()
+    console.log({ pokemons })
+})
+router.post('/', async (req, res) => {
+    try {
+        const poemonNew = req.body
+        const result = await pokeModel.create(poemonNew)
+
+        console.log({ result })
+        res.redirect('/pokemon')
+
+    } catch (error) {
+
+        console.log(error)
+        res.send('Error al crear el pokemon')
+
+    }
 })
 
-router.get('/create', async(req, res) => {
+router.get('/create', async (req, res) => {
     res.render('create', {})
 })
 
-router.get('/:name', async(req, res) => {
-    res.render('one', {pokemon: {
-        name: 'Pikachu', type: 'electric', Photo: ''
-    }})
+router.get('/:name', async (req, res) => {
+    try {
+        const { name } = req.params
+        const pokemon = await pokeModel.findOne({ name }).lean().exec()
+
+        res.render('one', { pokemon })
+    } catch (error) {
+        console.log(error)
+        res.send('Error to show pokemon')
+
+    }
 })
 
-router.delete('/:id', (req, res) => {
-    res.send('Pokemon is deleting...')
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        await pokeModel.deleteOne({ _id: id })
+
+        return res.json({ status: 'success' })
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 export default router
